b53a894c6f83039e2fe49c371520c21c3a65d909
Benny G <50448853+bgolfin@users.noreply.github.com>
2019-08-14 23:41:11 +0100

    Final Build for Interview
    Few fixes to implement on to the site to ensure correct titles display and overall functionality.
    - First test of GTM event to insert an alert near top of site - was unable to get this to work due to GTM informs need to be more ES6 standards.
    
    Jon's Website/about.html not needed for the demonstration.

---
+++ Pokemon/main.js
@@ -0,0 +1,16 @@
+/* const jumbotronTop = document.getElementById('jumbotronTop');
+const firstHead = document.getElementById('firstHead');
+
+console.log(jumbotronTop);
+console.log(firstHead);
+
+const rrrrMessage = document.createElement('p');
+rrrrMessage.textContent = "There's still tickets to WIN for the Global GO Fest next year! Enter on our Competition Page";
+
+//This is the same way as appendChild but in reverse
+function addToBeginning(parent, toInsert) {
+    const firstChild = parent.firstChild;
+    parent.insertBefore(toInsert, firstChild);
+}
+
+addToBeginning(jumbotronTop, rrrrMessage); */
